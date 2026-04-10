4b10c2ef004c3366f9370a758f6515f2a5b6f54b
Jesús Alfredo Hernández Alarcón <me@alfredohdz.io>
2026-04-09 20:24:08 -0600

    Genesis

---
+++ Scanly/ScanlyApp.swift
@@ -0,0 +1,14 @@
+//
+// Copyright © 2026 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ScanlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScanlyTests/ScanlyTests.swift
@@ -0,0 +1,16 @@
+//
+// Copyright © 2026 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import Testing
+@testable import Scanly
+
+struct ScanlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
